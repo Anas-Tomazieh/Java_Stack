2dad66585427fda70658ba7793e172ec2faf8506
Anas Tomazieh <1152325@student.birzeit.edu>
2021-06-28 16:39:05 +0300

    logim

---
+++ SpringBoot/login/loginreg/src/main/java/com/example/loginreg/repositories/UserRepo.java
@@ -0,0 +1,12 @@
+package com.example.loginreg.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.loginreg.models.User;
+
+@Repository
+public interface UserRepo extends CrudRepository<User, Long> {
+    User findByEmail(String email);
+
+}
