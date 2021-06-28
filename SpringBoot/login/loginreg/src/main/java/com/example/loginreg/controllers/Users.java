d1104834e28191c11fbe5344a75f4e468df4e54d
Anas Tomazieh <1152325@student.birzeit.edu>
2021-06-29 00:04:47 +0300

    ninja

--- SpringBoot/login/loginreg/src/main/java/com/example/loginreg/controllers/Users.java
+++ SpringBoot/login/loginreg/src/main/java/com/example/loginreg/controllers/Users.java
@@ -17,37 +17,41 @@ import com.example.loginreg.services.UserService;
 @Controller
 
 public class Users {
-private final UserService userService;
-    
-    public Users(UserService userService) {
-        this.userService = userService;
-    }
-    
-    @RequestMapping("/registration")
-    public String registerForm(@ModelAttribute("user") User user) {
-        return "registrationPage.jsp";
-    }
-    @RequestMapping("/login")
-    public String login() {
-        return "loginPage.jsp";
-    }
-    
-    @RequestMapping(value="/registration", method=RequestMethod.POST)
-    public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
-        // if result has errors, return the registration page (don't worry about validations just now)
-        // else, save the user in the database, save the user id in session, and redirect them to the /home route
-    	if (result.hasErrors())
+	private final UserService userService;
+
+	public Users(UserService userService) {
+		this.userService = userService;
+	}
+
+	@RequestMapping("/registration")
+	public String registerForm(@ModelAttribute("user") User user) {
+		return "registrationPage.jsp";
+	}
+
+	@RequestMapping("/login")
+	public String login() {
+		return "loginPage.jsp";
+	}
+
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
+		// if result has errors, return the registration page (don't worry about
+		// validations just now)
+		// else, save the user in the database, save the user id in session, and
+		// redirect them to the /home route
+		if (result.hasErrors())
 			return "registrationPage.jsp";
 		User u = userService.registerUser(user);
 		session.setAttribute("userId", u.getId());
 		return "redirect:/home";
-    }
-    
-    @RequestMapping(value="/login", method=RequestMethod.POST)
-    public String loginUser(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession session) {
-        // if the user is authenticated, save their user id in session
-        // else, add error messages and return the login page
-    	boolean isAuth = userService.authenticateUser(email, password);
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String loginUser(@RequestParam("email") String email, @RequestParam("password") String password, Model model,
+			HttpSession session) {
+		// if the user is authenticated, save their user id in session
+		// else, add error messages and return the login page
+		boolean isAuth = userService.authenticateUser(email, password);
 		if (isAuth) {
 			User u = userService.findByEmail(email);
 			session.setAttribute("userId", u.getId());
@@ -55,22 +59,23 @@ private final UserService userService;
 		}
 		model.addAttribute("error", "Invalid Credantials, Please try again");
 		return "loginPage.jsp";
-    }
-    
-    @RequestMapping("/home")
-    public String home(HttpSession session, Model model) {
-        // get user from session, save them in the model and return the home page
-    	Long userId = (Long) session.getAttribute("userId");
+	}
+
+	@RequestMapping("/home")
+	public String home(HttpSession session, Model model) {
+		// get user from session, save them in the model and return the home page
+		Long userId = (Long) session.getAttribute("userId");
 		User u = userService.findUserById(userId);
 		model.addAttribute("user", u);
 		return "homePage.jsp";
-    }
-    @RequestMapping("/logout")
-    public String logout(HttpSession session) {
-        // invalidate session
-        // redirect to login page
-    	session.invalidate();
+	}
+
+	@RequestMapping("/logout")
+	public String logout(HttpSession session) {
+		// invalidate session
+		// redirect to login page
+		session.invalidate();
 		return "redirect:/login";
-	
-    }
+
+	}
 }
